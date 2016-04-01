a816a47d2fe0ba4c2a0ecdb5206905160dad734d
Li-chao-qing <2356801841@qq.com>
2016-04-02 00:25:32 +0800

    lcq change course

--- pcweb/js/lcq_index.js
+++ pcweb/js/lcq_index.js
@@ -11,28 +11,50 @@ $('.lcq_co_class').css({position:"relative"});
 $(".lcq_co_class:eq(0)").css({top:100})
 $(".lcq_co_class:eq(1)").css({top:200})
 $(".lcq_co_class:eq(2)").css({top:300})
-
 var lcq_cont=500;
 var lcq_int=20;
 var lcq_flag=false;
+var lcq_flag1=false;
 var l_t;
-$(window).scroll(function(){
+$('.lcq_co_class').hover(function(){
+	
+	$('.lcq_cover').removeClass('lcq_course_hover');
+	$(this).find('.lcq_cover').addClass('lcq_course_hover');
+	$(this).find('.lcq_btn_m').addClass('lcq_btn_m_active');
+},function(){
+	$('.lcq_cover').removeClass('lcq_course_hover');
+	$('.lcq_btn_m').removeClass('lcq_btn_m_active');
+});
+
+$(".lcq_co_class").css({opacity:0});
+
+$(window).scroll(function(){	
 	if($(window).scrollTop()>=lcq_block+20){
-		$(".lcq_co_class:eq(0)").animate({ 
-		    top:0
-		}, 1000);
-		setTimeout(function(){
-			$(".lcq_co_class:eq(1)").animate({ 
-			    top:0
+		if(!lcq_flag1){
+			lcq_flag1=true;
+			$(".lcq_co_class:eq(0)").animate({ 
+			    top:0,
+			    opacity:1
 			}, 1000);
-		},50);
-		setTimeout(function(){
-			$(".lcq_co_class:eq(2)").animate({ 
-			    top:0
-			}, 1000);
-		},100);
-		
+			
+			setTimeout(function(){
+				
+				$(".lcq_co_class:eq(1)").animate({ 
+				    top:0,
+				     opacity:1
+				}, 1000);
+			},50);
+			setTimeout(function(){
+				
+				$(".lcq_co_class:eq(2)").animate({ 
+				    top:0,
+				     opacity:1
+				}, 1000);
+			},100);
+			
+		}
 	}
+	
 	////////////////
 	if($(window).scrollTop()>=lcq_block-68-300){
 		for(var i=0;i<4;i++){
@@ -62,7 +84,7 @@ $(window).scroll(function(){
 			$(".lcq_info").css({'font-size':zzz1+'px'})
 		},100);
 		}
-	},500);		
+	},1000);		
 
 	};
 })
