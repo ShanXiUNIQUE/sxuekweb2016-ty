3b06c7d8784036a61297c811acddc47bf300d345
Li-chao-qing <2356801841@qq.com>
2016-04-02 01:06:31 +0800

    lcq phone course

--- moblieweb/js/lcq_index.js
+++ moblieweb/js/lcq_index.js
@@ -8,3 +8,8 @@ var swiper = new Swiper('.swiper-container', {
         bulletClass : 'lcq_xdd',
         bulletActiveClass : 'lcq-btn-active',
 });
+var lcq_kuan=$('.lcq_co_special').width();
+$('.lcq_co_special').height(lcq_kuan);
+$('.lcq_x_word').css({'font-size':lcq_kuan/8+'px'});
+$('.lcq_d_word').css({'font-size':lcq_kuan/4+'px'});
+
