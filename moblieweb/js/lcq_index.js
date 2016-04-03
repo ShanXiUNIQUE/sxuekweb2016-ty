09db6591eebbba33966402a508d508d6ad445d79
Li-chao-qing <2356801841@qq.com>
2016-04-03 16:03:54 +0800

    lcq

--- moblieweb/js/lcq_index.js
+++ moblieweb/js/lcq_index.js
@@ -9,7 +9,12 @@ var swiper = new Swiper('.swiper-container', {
         bulletActiveClass : 'lcq-btn-active',
 });
 var lcq_kuan=$('.lcq_co_special').width();
+var lcq_kuan1=$('.lcq_bg').width();
 $('.lcq_co_special').height(lcq_kuan);
 $('.lcq_x_word').css({'font-size':lcq_kuan/8+'px'});
 $('.lcq_d_word').css({'font-size':lcq_kuan/4+'px'});
+$('.lcq_bg').height(lcq_kuan1/4);
+$('.lcq_ch_w').css({top:-lcq_kuan1/4+'px'});
+$('.lcq_one').css({"margin-top":-lcq_kuan1/4+'px'});
+
 
